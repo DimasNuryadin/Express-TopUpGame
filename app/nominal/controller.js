99cf23430f97f1dfaae143ce7c94028a2bd50dc1
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-03-11 19:44:24 +0700

    create method put, delete for nominal collection

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -36,7 +36,7 @@ module.exports = {
       let nominal = await Nominal({ coinName, coinQuantity, price })
       await nominal.save()
 
-      req.flash('alertMessage', "Data Berhasil ditambah!")
+      req.flash('alertMessage', "Data Nominal Berhasil ditambah!")
       req.flash('alertStatus', "success")
       res.redirect('/nominal')
 
@@ -46,51 +46,51 @@ module.exports = {
       res.redirect('/nominal')
     }
   },
-  // // Edit Nominal
-  // viewEdit: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;  // Ambil dari params
-  //     const nominal = await Nominal.findOne({ _id: id })   // Cek data berdasarkan id
-  //     res.render('admin/nominal/edit', { nominal })
-  //     // console.log(nominal)
-  //   } catch (err) {
-  //     req.flash('alertMessage', `${err.message}`)
-  //     req.flash('alertStatus', 'danger')
-  //     res.redirect('/nominal')
-  //   }
-  // },
-  // actionEdit: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
-  //     const { name } = req.body;
-  //     await Nominal.findOneAndUpdate({
-  //       _id: id
-  //     }, { name })
+  // Edit Nominal
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;  // Ambil dari params
+      const nominal = await Nominal.findOne({ _id: id })   // Cek data berdasarkan id
+      res.render('admin/nominal/edit', { nominal })
+      // console.log(nominal)
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/nominal')
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { coinName, coinQuantity, price } = req.body;
+      await Nominal.findOneAndUpdate({
+        _id: id
+      }, { coinName, coinQuantity, price })
 
-  //     req.flash('alertMessage', "Data Berhasil diubah!")
-  //     req.flash('alertStatus', "success")
-  //     res.redirect("/nominal")
+      req.flash('alertMessage', "Data Nominal Berhasil diubah!")
+      req.flash('alertStatus', "success")
+      res.redirect("/nominal")
 
-  //   } catch (err) {
-  //     req.flash('alertMessage', `${err.message}`)
-  //     req.flash('alertStatus', 'danger')
-  //     res.redirect('/nominal')
-  //   }
-  // },
-  // // Delete Nominal
-  // actionDelete: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
-  //     await Nominal.findByIdAndDelete({ _id: id });
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/nominal')
+    }
+  },
+  // Delete Nominal
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Nominal.findByIdAndDelete({ _id: id });
 
-  //     req.flash('alertMessage', "Data Berhasil dihapus!")
-  //     req.flash('alertStatus', "success")
-  //     res.redirect("/nominal")
+      req.flash('alertMessage', "Data Nominal Berhasil dihapus!")
+      req.flash('alertStatus', "success")
+      res.redirect("/nominal")
 
-  //   } catch (err) {
-  //     req.flash('alertMessage', `${err.message}`)
-  //     req.flash('alertStatus', 'danger')
-  //     res.redirect('/nominal')
-  //   }
-  // }
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/nominal')
+    }
+  }
 }
