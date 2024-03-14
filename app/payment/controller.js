49a71ed7d0e7badf433fe74d3ffa289d5b9621c7
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-03-15 01:14:15 +0700

    create method get and post for banks collection

---
+++ app/payment/controller.js
@@ -0,0 +1,98 @@
+const Payment = require('./model')
+const Bank = require('../bank/model')
+
+// Bisnis Logic Payment
+module.exports = {
+  index: async (req, res) => {
+    try {
+      // Connect Flash
+      const alertMessage = req.flash("alertMessage")  // Dapat dari app.js
+      const alertStatus = req.flash("alertStatus")
+      const alert = { message: alertMessage, status: alertStatus }
+
+      const payment = await Payment.find().populate("banks")
+      res.render("admin/payment/view_payment", {  // Mengambil file dari folder views
+        payment,      // Kirim data ke render views
+        alert
+      })
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/payment')
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      const banks = await Bank.find()
+      res.render("admin/payment/create", { banks })
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/payment')
+    }
+  },
+  // Create Payment
+  actionCreate: async (req, res) => {
+    try {
+      const { banks, type } = req.body;
+      let payment = await Payment({ banks, type })
+      await payment.save()
+
+      req.flash('alertMessage', "Data Payment Berhasil ditambah!")
+      req.flash('alertStatus', "success")
+      res.redirect('/payment')
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/payment')
+    }
+  },
+  // // Edit Payment
+  // viewEdit: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;  // Ambil dari params
+  //     const payment = await Payment.findOne({ _id: id })   // Cek data berdasarkan id
+  //     res.render('admin/payment/edit', { payment })
+  //     // console.log(payment)
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/payment')
+  //   }
+  // },
+  // actionEdit: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+  //     const { coinName, coinQuantity, price } = req.body;
+  //     await Payment.findOneAndUpdate({
+  //       _id: id
+  //     }, { coinName, coinQuantity, price })
+
+  //     req.flash('alertMessage', "Data Payment Berhasil diubah!")
+  //     req.flash('alertStatus', "success")
+  //     res.redirect("/payment")
+
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/payment')
+  //   }
+  // },
+  // // Delete Payment
+  // actionDelete: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+  //     await Payment.findByIdAndDelete({ _id: id });
+
+  //     req.flash('alertMessage', "Data Payment Berhasil dihapus!")
+  //     req.flash('alertStatus', "success")
+  //     res.redirect("/payment")
+
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/payment')
+  //   }
+  // }
+}
---
+++ app/payment/model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const paymentSchema = mongoose.Schema({
+  type: {
+    type: String,
+    require: [true, "Tipe pembayaran harus diisi!"]
+  },
+  status: {
+    type: String,
+    enum: ['Y', 'N'],
+    default: 'Y'
+  },
+  banks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Bank'
+  }]
+})
+
+module.exports = mongoose.model('Payment', paymentSchema) // Nominal : nama collection
