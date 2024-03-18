983491f6fe0f922f886f8d68cf8e9b8446bc7845
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-03-18 17:26:09 +0700

    get all category for signup user

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require('./model')
 const Voucher = require('../voucher/model')
+const Category = require('../category/model')
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -32,4 +33,12 @@ module.exports = {
       res.status(500).json({ message: err.message || `Internal server error` })
     }
   },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+      res.status(200).json({ data: category })
+    } catch (error) {
+      res.status(500).json({ message: err.message || `Internal server error` })
+    }
+  }
 }
--- app/player/router.js
+++ app/player/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage, detailPage } = require('./controller')  // Tempat Bisnis Logic
+const { landingPage, detailPage, category } = require('./controller')  // Tempat Bisnis Logic
 
 /* GET home page. */
 router.get('/landingpage', landingPage);
 router.get('/:id/detail', detailPage);
+router.get('/category', category);
 
 module.exports = router;
