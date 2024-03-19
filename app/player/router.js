f72c1c839a4442dfe5698808e892d676a0232f2e
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-03-19 22:22:26 +0700

    create api transaction detail

--- app/player/router.js
+++ app/player/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage, detailPage, category, checkout, history } = require('./controller')  // Tempat Bisnis Logic
+const { landingPage, detailPage, category, checkout, history, historyDetail } = require('./controller')  // Tempat Bisnis Logic
 
 // Middelware JWT
 const { isLoginPlayer } = require('../middleware/auth')
@@ -11,5 +11,6 @@ router.get('/:id/detail', detailPage);
 router.get('/category', category);
 router.post('/checkout', isLoginPlayer, checkout);
 router.get('/history', isLoginPlayer, history);
+router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 
 module.exports = router;
