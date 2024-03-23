860a0f731068cef6c561c7a46e5cce074a313889
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-03-24 01:23:25 +0700

    fix error cors

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ const URL = '/API/v1'
 
 // Cors
 app.use(cors())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
