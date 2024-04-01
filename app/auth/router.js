6fcff2362e13a7fee7083f443c23e53b667a75a8
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-04-02 02:22:15 +0700

    check error cors severs

--- app/auth/router.js
+++ app/auth/router.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { signUp, signIn } = require('./controller')  // Tempat Bisnis Logic
+const vercelCors = require('../middleware/vercel')
 
 // Multer
 const multer = require('multer')
 const os = require('os')
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
 /* GET home page. */
 router.post('/signup', multer({ dest: os.tmpdir() }).single('image'), signUp);
-router.post('/signin', allowCors(signIn))
+router.post('/signin', vercelCors, signIn)
 
 module.exports = router;
--- app/middleware/vercel.js
+++ app/middleware/vercel.js
@@ -15,9 +15,4 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
+module.exports = allowCors()
